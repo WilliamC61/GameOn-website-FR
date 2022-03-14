64972339e417657fcd304268f56cea309e4b67d4
WilliamC61 <william.charlet@wanadoo.fr>
2022-03-14 17:07:14 +0100

    addition of laptop
    based on desktop with only (bigger)mobile burger menu

--- menu.js
+++ menu.js
@@ -1,15 +1,31 @@
+const desktopkreakpoint = "1200px";
+
 function displayBurgerMenu(event) {
     event.preventDefault();
-    const burgerMenu = document.querySelector(".header_top-nav_ul");
+    const burgerMenu = document.querySelector(".header_top-nav_burger-menu");
     burgerMenu.style.visibility = "visible";
+}
+// if not on deskto, close the burger menu after 300ms
+function closeBurgerMenu(event) {
+    event.preventDefault();
+    if (!window.matchMedia("(min-width:" + " " + desktopkreakpoint + ")").matches) {
+        setTimeout(function () {
+            const burgerMenu = document.querySelector(".header_top-nav_burger-menu");
+            burgerMenu.style.visibility = "";
+        }, 300);
+    }
 }
 
 // identification of element on which events are listen
 const burgerMenuButton = document.querySelector(".header_top-nav_burger-menu-button");
-// const modalConfirmationCloseButton = document.querySelector(".modal_confirmation_close_button");
+const menuItemTable = document.querySelectorAll(".header_top-nav_ul_li");
 
 // Events to be listened
 burgerMenuButton.addEventListener("click", displayBurgerMenu);
+
+for (const menuItem of menuItemTable) {
+    menuItem.addEventListener("click", closeBurgerMenu);
+}
 // subscriptionFormSubmit.addEventListener("submit", subscriptionFormValidation);
 // modalHeaderModalQuitIcon.addEventListener("click", modalQuit);
 // modalConfirmationCloseButton.addEventListener("click", modalQuit);
