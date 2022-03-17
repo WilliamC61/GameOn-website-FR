9599442fcfe8ddcf4161b6efa9d78fc98847d091
WilliamC61 <william.charlet@wanadoo.fr>
2022-03-17 15:53:56 +0100

    Java script comment done

--- menu.js
+++ menu.js
@@ -1,31 +1,54 @@
-const desktopkreakpoint = "1200px";
+// ============================================================================
+// Burger menu managementcode
+// ============================================================================
 
+/**
+ * @function displayBurgerMenu(event) : display the burger nav menu
+ * @param {event} event : click event used to inhibate propagation
+ * @listens click event on burger menu icon
+ * Function is triggered by click on the burger menu icon displyed for all
+ * screen sizes except for desktop.
+ * Visibility is used as the menu if over (z-index = 1 the main page)
+ */
 function displayBurgerMenu(event) {
     event.preventDefault();
     const burgerMenu = document.querySelector(".header_top-nav_burger-menu");
     burgerMenu.style.visibility = "visible";
 }
-// if not on deskto, close the burger menu after 300ms
+
+/**
+ * @function closeBurgerMenu(event) : close the burger nav menu
+ * @param {*} event : click event, used to inhibate propagation
+ * @listens : click event on on one nav menu item
+ * Function is triggered by click on one nav menu item. a time-out is then
+ * set-up to hide the burger menu after 150ms.
+ * screen sizes except for desktop.
+ * Visibility is used as the menu if over (z-index = 1 the main page)
+ */
 function closeBurgerMenu(event) {
     event.preventDefault();
-    if (!window.matchMedia("(min-width:" + " " + desktopkreakpoint + ")").matches) {
-        setTimeout(function () {
-            const burgerMenu = document.querySelector(".header_top-nav_burger-menu");
-            burgerMenu.style.visibility = ""; // "" and not "hidden" to preserve default value for desktop
-        }, 150);
-    }
+    setTimeout(function () {
+        const burgerMenu = document.querySelector(".header_top-nav_burger-menu");
+        burgerMenu.style.visibility = ""; // "" and not "hidden" to preserve default value for desktop
+    }, 150);
 }
 
-// identification of element on which events are listen
+/**
+ *  Code executed at inital page display:
+ *  1/ Identification of the elements on which events are listen
+ *  2/ set-up of event listening
+ */
+
+// ──── identification of the elements on which events are listen ────────────────────
+// burgen menu icon
 const burgerMenuButton = document.querySelector(".header_top-nav_burger-menu-button");
+// list of the burger menu items
 const menuItemTable = document.querySelectorAll(".header_top-nav_ul_li");
 
-// Events to be listened
+// ──── Events to be listened ───────────────────────────────────────────────────
+// click on burger menu icon
 burgerMenuButton.addEventListener("click", displayBurgerMenu);
-
+// clik on all burger menu items
 for (const menuItem of menuItemTable) {
     menuItem.addEventListener("click", closeBurgerMenu);
 }
-// subscriptionFormSubmit.addEventListener("submit", subscriptionFormValidation);
-// modalHeaderModalQuitIcon.addEventListener("click", modalQuit);
-// modalConfirmationCloseButton.addEventListener("click", modalQuit);
--- modal.js
+++ modal.js
@@ -1,34 +1,89 @@
-// all unitary validaiton functions print or clear error message and return 0 if OK or 1 in, case of error
-// numeric value used to have addition => form is valid if sum of validation function == 0
+// ============================================================================
+// Modal management code
+// ============================================================================
 
-function firstnameValidation() {
+// ────────────────────────────────────────────────────────────────────────────
+// Verifcation functions used to validate the subscription form
+//
+// All unitary validation functions have the same structure:
+//     - if the field value is  valid
+//         - clear the associated error message
+//         - remove the "has error" class to remove the red border around
+//           the field
+//         - and return 0 for 0 error
+//     - else
+//         - put appropriate message in the associated error message
+//         - add the "has error" class to add a red border around the field
+//         - and return 1 for 1 error found
+//
+// the field values are retrieved by their id
+// the error message are retrieved by their subclass ( same as id with --prefix)
+//
+//
+// The global validaition function :
+//     - compute the number of error by adding the value returned by all the
+//       unitary validaiton function
+//     - trigger necessary actions depending on the result
+// ────────────────────────────────────────────────────────────────────────────
+
+/**
+ * firstnameValidation() : validate first name
+ *
+ * @returns:
+ *      0 (0 error) if firsname is at least 2 characters long
+ *      1 (error found) if not
+ *
+ * The function manages as well the associated error message field and the border
+ * around the input field.
+ */
+function firstNameValidation() {
     const errorMessageElement = document.querySelector(".--firstname");
     const element = document.getElementById("firstname");
 
     if (element.value.length < 2) {
         errorMessageElement.textContent = "Entrez au moins 2 caractères pour votre prénom";
-        element.classList.add("--erroneous");
+        element.classList.add("has-error");
         return 1; // error found
     }
     errorMessageElement.textContent = "";
-    element.classList.remove("--erroneous");
+    element.classList.remove("has-error");
     return 0; // no error
 }
 
+/**
+ * nameValidation() : validate first name
+ *
+ * @returns:
+ *      0 (0 error) if firsname is at least 2 characters long
+ *      1 (error found) if not
+ *
+ * The function manages as well the associated error message field and the border
+ * around the input field.
+ */
 function nameValidation() {
     const errorMessageElement = document.querySelector(".--name");
     const element = document.getElementById("name");
 
     if (element.value.length < 2) {
         errorMessageElement.textContent = "Entrez au moins 2 caractères pour votre nom";
-        element.classList.add("--erroneous");
+        element.classList.add("has-error");
         return 1;
     }
     errorMessageElement.textContent = "";
-    element.classList.remove("--erroneous");
+    element.classList.remove("has-error");
     return 0;
 }
 
+/**
+ * eMailValidation() : validate  eMail
+ *
+ * @returns:
+ *      0 (0 error) if eMail match email regex template
+ *      1 (error found) if not
+ *
+ * The function manages as well the associated error message field and the border
+ * around the input field.
+ */
 function eMailValidation() {
     const errorMessageElement = document.querySelector(".--email");
     const element = document.getElementById("email");
@@ -36,17 +91,28 @@ function eMailValidation() {
 
     if (!eMailTemplate.test(element.value)) {
         errorMessageElement.textContent = "Entrez un email valide";
-        element.classList.add("--erroneous");
+        element.classList.add("has-error");
         return 1;
     }
     errorMessageElement.textContent = "";
-    element.classList.remove("--erroneous");
+    element.classList.remove("has-error");
     return 0;
 }
 
-// check not only regex aceepting    only "/"" as separator, but as well nb of day per month
-// be careful the date format of input type = date is YYYY-MM-DD
-
+/**
+ * isDateValid(date) : check if the date in argument is a valid date.
+ * @param {*} date : the date to be tested
+ * @returns:
+ *      true if the date is valid
+ *      false if not
+ * * The expected format is "YYYY/MM/DD". the check is made in 4 pass:
+ *      1/ check of the date template used to split the date string
+ *      2/ check that month is in range 1..12
+ *      3/ check that the day is coherent with month (using an array)
+ *      4/ if date is februry 29th YYYY check that the year is a leap year
+ *         using Wikipedia definition
+ *
+ */
 function isDateValid(date) {
     const dateTemplate = /(\d{4})[- /](\d{1,2})[- /](\d{1,2})/;
     const nbDayPerMonth = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -71,36 +137,71 @@ function isDateValid(date) {
     return true;
 }
 
+/**
+ * dateOfBirthValidation() : validate birth date
+ *
+ * @returns:
+ *      0 (0 error) if given date is a valid date
+ *      1 (error found) if the date is of invalid format
+ * the "generic" function isDateValid(date) is used to check the date.
+ * the function manage as well the associated error message field and the border
+ * around the input field.
+ */
 function dateOfBirthValidation() {
     const errorMessageElement = document.querySelector(".--date-of-birth");
     const element = document.getElementById("date-of-birth");
     if (!isDateValid(element.value)) {
         errorMessageElement.textContent = "Entrez une date valide";
-        element.classList.add("--erroneous");
+        element.classList.add("has-error");
         return 1;
     }
     errorMessageElement.textContent = "";
-    element.classList.remove("--erroneous");
+    element.classList.remove("has-error");
     return 0;
 }
 
+/**
+ * playedTournamentNumberValidation() : validate the number of tournament played
+ *
+ * @returns:
+ *      0 (0 error) if the number is not valid
+ *      1 (error found) if not
+ *
+ * The check is made by testing : if the input is not empty (===""), and if not empty,
+ * if it is in the range 0..20. in case of error an appropriate error messge is displayed.
+ * The function manages as well the associated error message field and the border
+ * around the input field.
+ */
 function playedTournamentNumberValidation() {
     const errorMessageElement = document.querySelector(".--played-tournament-number");
     const element = document.getElementById("played-tournament-number");
     if (element.value === "") {
         errorMessageElement.textContent = "Entrez un nombre valide";
-        element.classList.add("--erroneous");
+        element.classList.add("has-error");
         return 1;
     } else if (element.value < 0 || element.value > 20) {
         errorMessageElement.textContent = "Entrez un nombre entre 0 et 20";
-        element.classList.add("--erroneous");
+        element.classList.add("has-error");
         return 1;
     }
     errorMessageElement.textContent = "";
-    element.classList.remove("--erroneous");
+    element.classList.remove("has-error");
     return 0;
 }
 
+/**
+ * locationValidation() : validate that a next tournament location is selected
+ *
+ * @returns:
+ *      0 (0 error) if one city is selected
+ *      1 (error found) if no city selected
+ *
+ * The check is made by testing if on of the radio button is selected.
+ * Test loop stops when one city is found selected. The fact that only one
+ * city is selectd is insured by radio button mechanism.
+ * The function manages as well the associated error message field and the border
+ * around the input field.
+ */
 function locationValidation() {
     const errorMessageElement = document.querySelector(".--input-location");
     const locationButtonTable = document.getElementsByClassName("subscription_form_input-radio");
@@ -114,7 +215,17 @@ function locationValidation() {
     return 1;
 }
 
-function termOfUseValidation() {
+/**
+ * termOfUseValidation() : validate that the terms of use checkbox is checked.
+ *
+ * @returns:
+ *      0 (0 error) if the checkbox is checked
+ *      1 (error found) if not
+ *
+ * The function manages as well the associated error message field and the border
+ * around the input field.
+ */
+function termsOfUseValidation() {
     const errorMessageElement = document.querySelector(".--terms-of-use");
     const element = document.getElementById("terms-of-use");
 
@@ -126,15 +237,36 @@ function termOfUseValidation() {
     return 0;
 }
 
+/**
+ *  subscriptionFormValidation() : Global validation of subscription form.
+ *
+ * @param {*} event : the submit event that is fully processed and not
+ *      propagated forward.
+ *
+ *  if the validation is OK
+ *      the confirmation form is displayed in replacement of the
+ *      subscription one
+ *  else
+ *      the subcription form is kept displayed and user can see the error
+ *      message(s) diplayed below the fied(s).
+ *
+ * The function is triggered by the submit event of the subscription form
+ * The test is made by adding the result of all the unitary validation
+ * functions.
+ * Note: numeric values are used as the use of boolean with the same
+ * structure (chain of && or ||) would not insure the  all tests performance.
+ *
+ */
+
 function subscriptionFormValidation(event) {
     event.preventDefault();
-    const errorNumber = firstnameValidation() +
+    const errorNumber = firstNameValidation() +
                                     nameValidation() +
                                     eMailValidation() +
                                     dateOfBirthValidation() +
                                     playedTournamentNumberValidation() +
                                     locationValidation() +
-                                    termOfUseValidation();
+                                    termsOfUseValidation();
     if (errorNumber === 0) {
         const modalSubscription = document.querySelector(".modal_subscription");
         modalSubscription.style.display = "none";
@@ -143,7 +275,19 @@ function subscriptionFormValidation(event) {
     }
 }
 
-function modalQuit(event) {
+/**
+ *  modalQuit() : close the modal and the lightbox and return to main page.
+ *
+ * The subscription, confirmation and modal are hidden (display: none).
+ * The modal header is kept untouched has hidden by the container modal.
+ * Subcription and confirmation are hidden to retrun back to initial
+ * HTML/CSS sate hidden, hidden.
+ * The light box is hidden as well using visibility:hidden.
+ *
+ * The function is triggered either by the click of the quit icon of the
+ * modal header or the "close" bitton of the confirmation.
+ */
+function modalQuit() {
     const modalSubscription = document.querySelector(".modal_subscription");
     modalSubscription.style.display = "none";
     const modalConfirmation = document.querySelector(".modal_confirmation");
@@ -154,15 +298,31 @@ function modalQuit(event) {
     lightbox.style.visibility = "hidden";
 }
 
+/**
+ *  subscriptionLaunch() : display lightbox and the subscription form.
+ *
+ * The light box is displeyed with visibility: visible.
+ * The subscription is displayed with display: flex.
+ * Nothing to do for the modal header
+ *
+ * The function is triggered by the click on the button "I subscribe".
+ */
 function subscriptionLaunch(event) {
     const lightbox = document.querySelector(".lightbox");
     lightbox.style.visibility = "visible";
     const modal = document.querySelector(".modal");
     modal.style.display = "flex";
     const modalSubscription = document.querySelector(".modal_subscription");
-    modalSubscription.style.display = "block";
+    modalSubscription.style.display = "flex";
 }
-// identification of element on which events are listen
+
+/**
+ *  Code executed at inital page display:
+ *  1/ Identification of the elements on which events are listen
+ *  2/ set-up of event listening
+ */
+
+// Identification of the elements on which events are listen
 const subcriptionLaunchButton = document.querySelector(".subscription-launch_button");
 const subscriptionFormSubmit = document.querySelector("#subscription-form");
 const modalHeaderModalQuitIcon = document.querySelector(".modal_header_modal-quit-icon");
